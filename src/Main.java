a3588909e047b5a6d442c5f13682b4adf0c74fbf
wartinsch <martin.schlachta1@gmail.com>
2023-07-26 23:22:38 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
     public class Main {
         public static void main(String[] args) {
-            int newScore = calculateScore("tim",500);
-            System.out.println("New score is: " + newScore);
-
-            calculateScore(75);
-            calculateScore(520255);
+//            int newScore = calculateScore("Martin",500);
+//            System.out.println("New score is: " + newScore);
+//
+//            calculateScore(75);
+//            calculateScore();
+            System.out.println("New score is " + calculateScore("Martin",500));
+            System.out.println("New score is " + calculateScore(10));
         }
 
         public static int calculateScore(String playerName, int score){
@@ -12,8 +14,20 @@
             return score * 1000;
         }
 
+//        public static int calculateScore(int score){
+//            System.out.println("Unnamed player scored " + score +" points.");
+//            return score * 1000;
+//        }
+
         public static int calculateScore(int score){
-            System.out.println("Unnamed player scored " + score +" points.");
-            return score * 1000;
+            return calculateScore("Anonymous",score);
+
         }
+
+        public static int calculateScore(){
+            System.out.println("No player name, no player score.");
+            return 0;
+        }
+
+
     }
