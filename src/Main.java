ddad9ec7f0996c1fc480a2d336f1f1936445428c
wartinsch <martin.schlachta1@gmail.com>
2023-07-26 23:11:53 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+    public class Main {
+        public static void main(String[] args) {
+            int newScore = calculateScore("tim",500);
+            System.out.println("New score is: " + newScore);
+
+            calculateScore(75);
+            calculateScore(520255);
+        }
+
+        public static int calculateScore(String playerName, int score){
+            System.out.println("Player " + playerName + " scored " + score +" points.");
+            return score * 1000;
+        }
+
+        public static int calculateScore(int score){
+            System.out.println("Unnamed player scored " + score +" points.");
+            return score * 1000;
+        }
+    }
